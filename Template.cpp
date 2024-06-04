736b759b826a6cb928448e07c2dfc5dd86500d32
cryoelite <arvindsagar702@gmail.com>
2024-06-04 19:03:42 +0530

    Setup Complete

--- Template.cpp
+++ Template.cpp
@@ -1,61 +1,57 @@
-// https://cses.fi/problemset/task/1660
+// <url>
 
-// Local Mode (for local tests, take input from files), or comment this to be in Submission mode (take input from CLI)
-// #define LOCAL
+// Local Mode (for local tests, take input from files), or comment this to be in
+// Submission mode (take input from CLI) #define ForceLOCAL
 
 // Headers
 #include <algorithm>
 #include <array>
 #include <complex>
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
 #include <functional>
+#include <iomanip>
 #include <iostream>
 #include <map>
 #include <memory>
+#include <numeric>
 #include <queue>
 #include <set>
 #include <stack>
 #include <stdio.h>
+#include <string>
 #include <string_view>
 #include <tuple>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
-#include <numeric>
-#include <iomanip>
-#ifdef LOCAL
-#include "../../Helpers/Easybench/Easybench.h"
-#endif // LOCAL
 
 namespace Definitions
 
 {
-  // Defines
+// Defines
 #define int long long
 #define double long double
 #define LOG(x) static_cast<int>(std::floor(std::log2(x)))
-#define IOS                              \
-  std::ios_base::sync_with_stdio(false); \
-  std::cin.tie(0);                       \
+#define IOS                                                                    \
+  std::ios_base::sync_with_stdio(false);                                       \
+  std::cin.tie(0);                                                             \
   std::cout.tie(0);
-  template <typename... T>
-  void INPUT(T &...args) { ((std::cin >> args), ...); }
-  template <typename... T>
-  void OUTPUT(T &...args)
-  {
-    ((std::cout << args << " "), ...);
-    std::cout << "\n";
+template <typename... T> void INPUT(T &...args) { ((std::cin >> args), ...); }
+template <typename... T> void OUTPUT(T &...args) {
+  ((std::cout << args << " "), ...);
+  std::cout << "\n";
+}
+#define ARR_INPUT(arr, x)                                                      \
+  for (int i{0}; i < x; ++i) {                                                 \
+    int y;                                                                     \
+    std::cin >> y;                                                             \
+    arr.push_back(y);                                                          \
   }
-#define ARR_INPUT(arr, x)    \
-  for (int i{0}; i < x; ++i) \
-  {                          \
-    int y;                   \
-    std::cin >> y;           \
-    arr.push_back(y);        \
-  }
-#define ARR_OUTPUT(arr, sep) \
-  for (auto &elem : arr)     \
-  {                          \
-    cout << elem << sep;     \
+#define ARR_OUTPUT(arr, sep)                                                   \
+  for (auto &elem : arr) {                                                     \
+    cout << elem << sep;                                                       \
   }
 
 #define cast(i) static_cast<int>(i)
@@ -67,66 +63,92 @@ namespace Definitions
 #define tiii std::tuple<int, int, int>
 #define mmi std::make_move_iterator
 
-  // Constants
-  constexpr int cmod10_e9_7{1000000007}; // const mod 10e9^7
-  constexpr int cIN{200005};             // const N (fits in int)
-  constexpr int cLN{1000005};            // const long N(fits in long long)
+// Constants
+constexpr int cmod10_e9_7{1000000007}; // const mod 10e9^7
+constexpr int cIN{200005};             // const N (fits in int)
+constexpr int cLN{1000005};            // const long N(fits in long long)
 
-  // Type Aliases
-  using usi = std::unordered_set<int>;
-  using umii = std::unordered_map<int, int>;
-  using vi = std::vector<int>;
-  using si = std::set<int>;
-  using sd = std::set<double>;
-  using vvi = std::vector<vi>;
-  using pivi = std::pair<int, vi>; // first is node's value and second is node's
-                                   // adjacent elements
-  using pii = std::pair<int, int>;
-  using vpii = std::vector<pii>; // edge list
-  using vtiii = std::vector<tiii>;
-  using vvtiii = std::vector<vtiii>; // adjacency list with adjacent node id, edge
-                                     // weight and an extra value.
-  using vpivi = std::vector<pivi>;
-  using vvpii =
-      std::vector<vpii>; // adjacency list with edge weights, the pii has first as
-                         // node id and second as the edge weight
-  using mii = std::map<int, int>;
-  using vmii = std::vector<mii>;
-  using vb = std::vector<bool>; // vector<bool> is a special explicit definition
-                                // of vector and behaves more like a bitset than a
-                                // vector, also it is faster than array<bool>
-                                // https://stackoverflow.com/a/55762317/13036358
-  using vvb = std::vector<vb>;
-  using ri = std::reverse_iterator<vi::iterator>;
-  using ski = std::stack<int>;
-  using CD = std::complex<double>;
-  using CI = std::complex<int>; // DEPRECATED
-  using pqd = std::priority_queue<double>;
-  using pqi = std::priority_queue<int>;
-  using pqpii = std::priority_queue<pii>;
-  using vcd = std::vector<CD>;
-  using vci = std::vector<CI>;
-  using pcd = std::pair<CD, CD>;
-  using pci = std::pair<CI, CI>;
-  using vpcd = std::vector<pcd>;
-  using vpci = std::vector<pci>;
-  using vs = std::vector<std::string_view>;
+// Type Aliases
+using usi = std::unordered_set<int>;
+using umii = std::unordered_map<int, int>;
+using vi = std::vector<int>;
+using si = std::set<int>;
+using sd = std::set<double>;
+using vvi = std::vector<vi>;
+using pivi = std::pair<int, vi>; // first is node's value and second is node's
+                                 // adjacent elements
+using pii = std::pair<int, int>;
+using vpii = std::vector<pii>; // edge list
+using vtiii = std::vector<tiii>;
+using vvtiii = std::vector<vtiii>; // adjacency list with adjacent node id, edge
+                                   // weight and an extra value.
+using vpivi = std::vector<pivi>;
+using vvpii =
+    std::vector<vpii>; // adjacency list with edge weights, the pii has first as
+                       // node id and second as the edge weight
+using mii = std::map<int, int>;
+using vmii = std::vector<mii>;
+using vb = std::vector<bool>; // vector<bool> is a special explicit definition
+                              // of vector and behaves more like a bitset than a
+                              // vector, also it is faster than array<bool>
+                              // https://stackoverflow.com/a/55762317/13036358
+using vvb = std::vector<vb>;
+using ri = std::reverse_iterator<vi::iterator>;
+using ski = std::stack<int>;
+using CD = std::complex<double>;
+using CI = std::complex<int>; // DEPRECATED
+using pqd = std::priority_queue<double>;
+using pqi = std::priority_queue<int>;
+using pqpii = std::priority_queue<pii>;
+using vcd = std::vector<CD>;
+using vci = std::vector<CI>;
+using pcd = std::pair<CD, CD>;
+using pci = std::pair<CI, CI>;
+using vpcd = std::vector<pcd>;
+using vpci = std::vector<pci>;
+using vs = std::vector<std::string_view>;
 
+} // namespace Definitions
+
+namespace SetupEnvironment {
+using namespace Definitions;
+
+bool isLocalMode() {
+#ifdef ForceLOCAL
+  return true;
+#else
+  std::filesystem::path envFilePath{"../../.env"};
+
+  if (std::filesystem::is_regular_file(envFilePath)) {
+    // Env var file exists
+    std::ifstream file{
+
+        std::ifstream(envFilePath, std::ios::in | std::ios::binary)};
+
+    if (file.is_open()) {
+      // Env var file isn't already open
+      std::string envVar{std::istreambuf_iterator<char>(file),
+                         std::istreambuf_iterator<char>()};
+      // Env variable retrieved
+
+      char *envVarValue{std::getenv(envVar.c_str())};
+
+      if (envVarValue != nullptr) {
+        // Env var exists, now checking if it is true
+        return envVarValue == std::string("true");
+      }
+    }
+  }
+  return false;
+#endif
 }
 
-namespace SetupEnvironment
-{
-  using namespace Definitions;
+void setupIO() {
+  IOS;
 
-  // Function Bodies
-  void setupIO()
-  {
-    IOS;
-
+  if (isLocalMode()) {
     // I/O Stream pointed at local text files
-#ifdef LOCAL
-
-#ifndef freopen_s // windows
+#ifdef freopen_s // windows
     FILE *inpStream;
     FILE *outStream;
 
@@ -134,73 +156,57 @@ namespace SetupEnvironment
     freopen_s(&outStream, "output.txt", "w", stdout);
 
 #else // linux
-    freopen64("input.txt", "r", stdin);
-    freopen64("output.txt", "w", stdout);
-
-#endif
-
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif
   }
-
 }
 
-namespace Solution
-{
-  using namespace Definitions;
-  using namespace SetupEnvironment;
-  using namespace std;
-  void start();
-  void initialize();
-  void compute();
-  void output();
+} // namespace SetupEnvironment
 
-  int testCases{1};
+namespace Solution {
+using namespace Definitions;
+using namespace SetupEnvironment;
+using namespace std;
+void start();
+void initialize();
+void compute();
+void output();
 
-  int n{};
-  int x{};
-  vi arr{};
-  int result{2};
+int testCases{1};
 
-  void start()
-  {
-    setupIO();
-    // INPUT(testCases);
-    while (testCases-- > 0)
-    {
-      initialize();
-      compute();
-      output();
-    }
+int n{};
+int x{};
+vi arr{};
+int result{2};
+
+void start() {
+  // INPUT(testCases);
+  while (testCases-- > 0) {
+    initialize();
+    compute();
+    output();
   }
-
-  void initialize()
-  {
-  }
-
-  void compute()
-  {
-  }
-
-  void output()
-  {
-    cout << result;
-    cout << '\n';
-  }
-
 }
 
-#define STARTLOCAL Solution::start();
+void initialize() {}
 
-signed main()
-{
-#ifdef LOCAL
-  EasyBench eb{};
-#endif
+void compute() {}
 
-  STARTLOCAL;
+void output() {
+  cout << result;
+  cout << '\n';
+}
 
-#ifdef LOCAL
-  eb.showresult();
-#endif
+} // namespace Solution
+
+#define SOLVE                                                                  \
+  SetupEnvironment::setupIO();                                                 \
+  Solution::start();
+
+signed main() {
+
+  SOLVE;
+
   return 0;
 }
---
+++ setup_new_problem.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "Setting up a new problem directory"
+echo "Problem No. ?"
+read -r problem_number
+
+echo "Problem Domain ? (HR: HackerRank, CSES: CSES, LC: LeetCode, CF: CodeChef)"
+read -r problem_domain
+
+echo "Problem ID on the domain ?"
+read -r problem_id
+
+problem_dir="Problems/${problem_number}_${problem_domain}_${problem_id}/"
+echo "Create problem directory: \"$problem_dir\" ? (y/Y: Yes, N/n: no)"
+read -r should_create
+
+if [[ "$should_create" != y ]] && [[ "$should_create" != Y ]]; then
+    echo "User input declines creation, exiting..."
+    exit
+fi
+
+echo "Creating problem Directory"
+mkdir -p "${problem_dir}"
+
+echo "Copying Template.cpp"
+cp Template.cpp "${problem_dir}problem.cpp"
+
+echo "Creating input.txt and output.txt"
+touch "${problem_dir}input.txt"
+touch "${problem_dir}output.txt"
+
+echo "Creating compile_and_run.sh"
+touch "${problem_dir}compile_and_run.sh"
+
+echo "Defining compile_and_run.sh"
+cat >>"${problem_dir}compile_and_run.sh" <<'END'
+#!/bin/bash
+echo "Getting env variable"
+env_var=$(<../../.env)
+export "${env_var?}"=true
+clang++ problem.cpp -o problem -std=c++20 && problem.exe
+END
+echo "compile_and_run.sh created successfully"
+
+echo "New Problem Setup Complete"
+exit
